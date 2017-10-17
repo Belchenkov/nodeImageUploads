1584bc43fd40aa18bf4f37dec6c125d4e3a59188
Belchenkov <u608110@gmail.com>
2017-10-17 07:28:14 +0300

    Upload a Pictire

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ const multer = require('multer');
 const ejs = require('ejs');
 const path = require('path');
 
+// Set Storage Engine
+const storage = multer.diskStorage({
+    destination: './public/uploads/',
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+// Init Upload
+const upload = multer({
+    storage
+}).single('myImage');
+
 // Init app
 const app = express();
 
-const port = 3003;
+const port = 3004;
 
 // EJS
 app.set('view engine', 'ejs');
@@ -16,6 +29,19 @@ app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if (err) {
+            res.render('index', {
+                msg: err
+            });
+        } else {
+            console.log(req.file);
+            res.send('test');
+        }
+    });
 });
 
 app.listen(port, () => {
