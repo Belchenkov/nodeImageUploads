fd9ae9194379952180bdbe17d8673b3bc4f1b882
Belchenkov <u608110@gmail.com>
2017-10-17 07:00:12 +0300

    Create UI with Materialize

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ const app = express();
 
 const port = 3003;
 
+// EJS
+app.set('view engine', 'ejs');
+
+// Public Folder
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
 app.listen(port, () => {
     console.log(`Server Started on http://localhost:${port}`);
 });
